a1b9da476a3d9211981efdd448782a4764c8985e
shravan varayogula <shravan.varayogula@gmail.com>
2023-09-09 21:03:06 +0530

    updated git ignore for client side node modules

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ExpenseTracker} from './components/Expense-Tracker';
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseTracker></ExpenseTracker>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Expense-Tracker.tsx
@@ -0,0 +1,11 @@
+// We use JSX Fragements within this ts file and we use the tsx extension.
+
+const ExpenseTracker = () => {
+    return (
+        <div>
+            Expense Tracker Created..
+        </div>
+    )
+}
+
+export  {ExpenseTracker};
